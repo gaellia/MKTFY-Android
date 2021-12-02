2ab5f4fab4ea831ee10fb7be18247abf6b6d03bc
Kevin Vo <25157215+gaellia@users.noreply.github.com>
2021-12-02 04:00:31 -0700

    outline for purchase list

--- app/src/main/java/com/launchpad/mktfy_android/ui/screens/myPurchases/MyPurchases.kt
+++ app/src/main/java/com/launchpad/mktfy_android/ui/screens/myPurchases/MyPurchases.kt
@@ -1,13 +1,21 @@
 package com.launchpad.mktfy_android.ui.screens.myPurchases
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.launchpad.mktfy_android.ui.components.Header
+import com.launchpad.mktfy_android.ui.theme.LightGrayBackground
 
 
 @Composable
@@ -56,7 +64,18 @@ private fun MyPurchasesContent(
         )
 
         // Purchases
-        Text("Welcome to your purchases")
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(LightGrayBackground),
+            contentPadding = PaddingValues(top = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            content = {
+                item {
+                    Text("Welcome to your purchases")
+                }
+            }
+        )
     }
 }
 
